84b8c0233c2cd185bfd11ca19c84ed16f59de3d3
Sailfish <l_how@live.com@b421a9fe-b5b2-4ef2-9de5-e03812cba923>
2016-08-31 00:46:44 +0000

    no commit message

---
+++ sql/updata_character_set.sql
@@ -0,0 +1,5 @@
+alter table  collection convert to character set utf;
+alter table  comment    convert to character set utf8;
+alter table  downloadtable convert to character set utf8;
+alter table  musicmenu  convert to character set utf8;
+alter table  usertable convert to character set utf8;
