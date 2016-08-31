a678636816eee9646e32191483e7734aa73c66b0
Sailfish <l_how@live.com@b421a9fe-b5b2-4ef2-9de5-e03812cba923>
2016-08-31 00:48:33 +0000

    sql

--- sql/download_menu_table.sql
+++
@@ -1,7 +0,0 @@
-use music;
-create table downloadtable(
-    downloda_id  smallint primary key auto_increment, /*下载ID*/
-    User_ID      int not null,                        /*用户ID*/
-    Music_ID     int not null                         /*歌曲ID*/
-    
-);
