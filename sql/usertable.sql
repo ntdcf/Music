58ea33659da625ac7c48eb6ebb1ce18d17a0d392
Sailfish <l_how@live.com>
2016-08-31 00:59:25 +0000

    sql

---
+++ sql/usertable.sql
@@ -0,0 +1,2 @@
+INSERT INTO `usertable` VALUES (1, 'is_user_test', '123456', 'test_user', '1', 18, '1995-6-24', 0);
+INSERT INTO `usertable` VALUES (2, 'is_admin', '123456', 'test_admin', '1', 18, '1995-6-24', 1);
