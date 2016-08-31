6e1e7b6dda5ec342a64c23582cab73c8c4b6b0a0
Sailfish <l_how@live.com>
2016-08-31 00:48:33 +0000

    sql

--- sql/Comment_menu_table.sql
+++
@@ -1,9 +0,0 @@
-use music;
-create table comment(
-
-	Comment_ID  int primary key auto_increment,/*评论ID*/
-	User_ID      int not null,                 /*用户ID*/
-    Music_ID     int not null,                 /*歌曲ID*/
-    comment_Info  varchar(200) not null,        /*评论内容*/
-    comment_count smallint not null
-);
